0150c7ecc710d834d24564ecbb066470c12584ae
Jwbranch4 <87912530+Jwbranch4@users.noreply.github.com>
2021-11-15 00:02:40 -0600

    created dashboard-routes and started javascript for frontend

--- models/User.js
+++ models/User.js
@@ -34,7 +34,7 @@ User.init(
     hooks: {
       async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return nerwUserData;
+        return newUserData;
       },
       async beforeUpdate(updatedUserData) {
         updatedUserData.password = await bcrypt.hash(
